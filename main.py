9320027a3a1c734121588f8c9c5b2bde7f4457a6
Mika Mika <mikamika@MikadeMacBook-Pro.local>
2024-11-06 00:03:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+def bio(name,born,pronoun):
+    age = 2024-born
+    future = born+67
+    print("----------------------------------------")
+    print(name.capitalize(), "was born in", born)
+    print(pronoun.capitalize(), "will turn", age, "years old this year")
+    print(pronoun.capitalize(), "will be 67 in the year", future)
+    print("----------------------------------------")
+
+bio("stephen",1984,"he")
+bio("mary",1990,"she")
+bio("jane",2000,"she")
+
+quit()
